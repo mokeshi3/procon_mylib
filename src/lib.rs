38e73fa3c7fe236fc1e3b230db363c07b87b595d
mokeshi34649 <mokeshi34649@gmail.com>
2021-06-30 21:16:39 +0900

    lcm

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,10 @@ fn gcd(a: usize, b: usize) -> usize {
     gcd(greater, smaller)
 }
 
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,6 +27,15 @@ mod tests {
         assert_eq!(gcd(12, 16), 4);
         assert_eq!(gcd(16, 12), 4);
         assert_eq!(gcd(123, 456), 3);
+    }
+
+    #[test]
+    fn test_lcm() {
+        assert_eq!(lcm(2, 2), 2);
+        assert_eq!(lcm(2, 4), 4);
+        assert_eq!(lcm(12, 10), 60);
+        assert_eq!(lcm(6, 7), 42);
+        assert_eq!(lcm(123, 456), 18696);
     }
 
     #[test]
